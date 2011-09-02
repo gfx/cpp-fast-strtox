a8af35d83651ee9a61e7a6ea8f1c589ebc125edf
Fuji Goro <fuji.goro@dena.jp>
2011-09-02 18:22:36 +0900

    Add benchmark

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ test: fast_strtox.h test.cpp
 	g++ -O3 test.cpp -o test
 	./test
 
+benchmark: fast_strtox.h benchmark.cpp
+	g++ -O3 benchmark.cpp -o benchmark
+	./benchmark
+	clang++ -O3 benchmark.cpp -o benchmark
+	./benchmark
+
---
+++ benchmark.cpp
@@ -0,0 +1,53 @@
+#include <boost/timer.hpp>
+#include <cstdlib>
+#include <iostream>
+
+#include "fast_strtox.h"
+
+#undef strtof
+#undef strtod
+#undef strtol
+
+int main() {
+    const char* const pistr  = "3.14159265358979323846264338327950288,foo";
+    const char* const ansstr = "42,foo";
+    {
+        boost::timer t;
+        for(int i = 0; i < (1 * 1000 * 1000); ++i) {
+            char* tmp;
+            float f = std::strtof(pistr, &tmp);
+            (void)f;
+        }
+        std::cout << "std::strtof(): " << t.elapsed() << std::endl;
+    }
+    {
+        boost::timer t;
+        for(int i = 0; i < (1 * 1000 * 1000); ++i) {
+            char* tmp;
+            float f = fast_strtof(pistr, &tmp);
+            (void)f;
+        }
+        std::cout << "fast_strtof(): " << t.elapsed() << std::endl;
+    }
+    {
+        boost::timer t;
+        for(int i = 0; i < (1 * 1000 * 1000); ++i) {
+            char* tmp;
+            float f = std::strtol(ansstr, &tmp, 10);
+            (void)f;
+        }
+        std::cout << "std::strtol(): " << t.elapsed() << std::endl;
+    }
+    {
+        boost::timer t;
+        for(int i = 0; i < (1 * 1000 * 1000); ++i) {
+            char* tmp;
+            float f = fast_strtol(ansstr, &tmp, 10);
+            (void)f;
+        }
+        std::cout << "fast_strtol(): " << t.elapsed() << std::endl;
+    }
+
+    return 0;
+}
+
